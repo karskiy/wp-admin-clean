bc4d36ac0da98b51df57ad66f3716f5b8292ce3d
Evgeniy Karskiy <karskiy@gmail.com>
2017-06-03 00:07:28 +0600

    Update wp-admin-clean.php

--- wp-admin-clean.php
+++ wp-admin-clean.php
@@ -14,6 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+// Удаляем метабоксы с информацией на страничке Консоль
 function krs_remove_dashboard_meta() {
         remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'normal' );
         remove_meta_box( 'dashboard_plugins', 'dashboard', 'normal' );
@@ -26,3 +27,22 @@ function krs_remove_dashboard_meta() {
         remove_meta_box( 'dashboard_activity', 'dashboard', 'normal');//since 3.8
 }
 add_action( 'admin_init', 'krs_remove_dashboard_meta' );
+
+// Удаляем в разделе Виджеты виджеты по умолчанию, чтоб глаза не мазолили ))
+function krs_remove_calendar_widget() {
+	unregister_widget('WP_Widget_Pages'); // Pages Widget
+	unregister_widget('WP_Widget_Calendar'); // Calendar Widget
+	unregister_widget('WP_Widget_Archives'); // Archives Widget
+	unregister_widget('WP_Widget_Links'); // Links Widget
+	unregister_widget('WP_Widget_Meta'); // Meta Widget
+	unregister_widget('WP_Widget_Search'); // Search Widget
+	//unregister_widget('WP_Widget_Text'); // Text Widget
+	unregister_widget('WP_Widget_Categories'); // Categories Widget
+	unregister_widget('WP_Widget_Recent_Posts'); // Recent Posts Widget
+	unregister_widget('WP_Widget_Recent_Comments'); // Recent Comments Widget
+	unregister_widget('WP_Widget_RSS'); // RSS Widget
+	unregister_widget('WP_Widget_Tag_Cloud'); // Tag Cloud Widget
+	//unregister_widget('WP_Nav_Menu_Widget'); // Menus Widget	
+}
+
+add_action( 'widgets_init', 'krs_remove_calendar_widget' );
