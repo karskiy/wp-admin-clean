c3b21d602cca31945296ed4a832337c9a0467d46
Evgeniy Karskiy <karskiy@gmail.com>
2017-06-03 00:10:20 +0600

    Update wp-admin-clean.php

--- wp-admin-clean.php
+++ wp-admin-clean.php
@@ -44,5 +44,22 @@ function krs_remove_calendar_widget() {
 	unregister_widget('WP_Widget_Tag_Cloud'); // Tag Cloud Widget
 	//unregister_widget('WP_Nav_Menu_Widget'); // Menus Widget	
 }
-
 add_action( 'widgets_init', 'krs_remove_calendar_widget' );
+
+// Удаляем бесполезные пункты меню из админки
+function krs_remove_menus(){
+  
+  remove_menu_page( 'index.php' );                  //Dashboard
+  remove_menu_page( 'jetpack' );                    //Jetpack* 
+  remove_menu_page( 'edit.php' );                   //Posts
+  remove_menu_page( 'upload.php' );                 //Media
+  remove_menu_page( 'edit.php?post_type=page' );    //Pages
+  remove_menu_page( 'edit-comments.php' );          //Comments
+  remove_menu_page( 'themes.php' );                 //Appearance
+  remove_menu_page( 'plugins.php' );                //Plugins
+  remove_menu_page( 'users.php' );                  //Users
+  remove_menu_page( 'tools.php' );                  //Tools
+  remove_menu_page( 'options-general.php' );        //Settings
+  
+}
+add_action( 'admin_menu', 'krs_remove_menus' );
